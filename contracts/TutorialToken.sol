ea061efd3a520acc2ad33f2a8d7f36d356b74148
Hans Wang <chihan09nc@gmail.com>
2018-04-29 12:57:55 -0700

    Changed identation

--- contracts/TutorialToken.sol
+++ contracts/TutorialToken.sol
@@ -3,13 +3,13 @@ pragma solidity ^0.4.18;
 import "zeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
 
 contract TutorialToken is StandardToken {
-  string public name = "TutorialToken";
-  string public symbol = "TT";
-  uint public decimals = 2;
-  uint public INITIAL_SUPPLY = 12000;
+    string public name = "TutorialToken";
+    string public symbol = "TT";
+    uint public decimals = 2;
+    uint public INITIAL_SUPPLY = 12000;
 
-  function TutorialToken() public {
-    totalSupply_ = INITIAL_SUPPLY;
-    balances[msg.sender] = INITIAL_SUPPLY;
-  }
+    function TutorialToken() public {
+        totalSupply_ = INITIAL_SUPPLY;
+        balances[msg.sender] = INITIAL_SUPPLY;
+    }
 }
